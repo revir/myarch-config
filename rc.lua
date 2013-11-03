cc64299a11c632d3c288db1eeeb447be0c30e103
revir <revir.qing@gmail.com>
2013-11-03 10:48:57 +0800

    as lilyterm cannot open multi windows, so set xterm to the default.

--- rc.lua
+++ rc.lua
@@ -45,7 +45,8 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "lilyterm"
+terminal = "xterm"
+lilyterm = "lilyterm"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
@@ -366,6 +367,7 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+	awful.key({ modkey, "Shift"   }, "Return", function () awful.util.spawn(lilyterm) end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
